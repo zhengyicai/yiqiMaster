755419988000f8f240aada5dd8c2b34cc3d628cb
1010172623@qq.com <1010172623@qq.com>
2019-07-26 00:13:25 +0800

    ddd

---
+++ qzi-cms-base/qzi-cms-common/src/main/java/com/qzi/cms/common/vo/UseEquipmentTitleVo.java
@@ -0,0 +1,160 @@
+package com.qzi.cms.common.vo;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+/**
+ * 设备ip and 端口
+ * Created by Administrator on 2019/3/19.
+ */
+
+
+
+
+public class UseEquipmentTitleVo {
+    /**
+     * 主键编号
+     */
+    @Id
+    private String id;
+
+    private String equipmentId;
+
+    private String fileId;
+
+
+
+
+    /**
+     * 名称 规格 单价 单位 简介
+     */
+    private String name;
+
+
+
+    private String  norms;
+
+
+    private String  price;
+
+    private String  unit;
+
+    private String  detail;
+
+    private String  remark;
+
+    private String  state;
+
+
+
+
+    private Date createTime;
+
+    private String  fileUrl;
+    private String  equipmentNo;
+
+    public String getFileUrl() {
+        return fileUrl;
+    }
+
+    public void setFileUrl(String fileUrl) {
+        this.fileUrl = fileUrl;
+    }
+
+    public String getEquipmentNo() {
+        return equipmentNo;
+    }
+
+    public void setEquipmentNo(String equipmentNo) {
+        this.equipmentNo = equipmentNo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEquipmentId() {
+        return equipmentId;
+    }
+
+    public void setEquipmentId(String equipmentId) {
+        this.equipmentId = equipmentId;
+    }
+
+    public String getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(String fileId) {
+        this.fileId = fileId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNorms() {
+        return norms;
+    }
+
+    public void setNorms(String norms) {
+        this.norms = norms;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
---
+++ qzi-cms-base/qzi-cms-server/src/main/java/com/qzi/cms/server/mapper/UseEquipmentTitleMapper.java
@@ -0,0 +1,46 @@
+/* 
+ * 文件名：UseRoomMapper.java  
+ * 版权：Copyright 2016-2017 炎宝网络科技  All Rights Reserved by
+ * 修改人：邱深友  
+ * 创建时间：2017年7月6日
+ * 版本号：v1.0
+*/
+package com.qzi.cms.server.mapper;
+
+import com.qzi.cms.common.po.UseEquipmentPo;
+import com.qzi.cms.common.po.UseEquipmentPortPo;
+import com.qzi.cms.common.po.UseEquipmentTitlePo;
+import com.qzi.cms.common.vo.UseEquipmentTitleVo;
+import com.qzi.cms.server.base.BaseMapper;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+/**
+ * 设备标签
+ * @author qsy
+ * @version v1.0
+ * @date 2017年7月6日
+ */
+public interface UseEquipmentTitleMapper extends BaseMapper<UseEquipmentTitlePo>{
+
+//	@Update("update use_equipment_title set ips=#{ips},port=#{port},createTime = now()  where equipmentNo=#{equipmentNo}")
+//	public void update(@Param("ips") String ips, @Param("port") String port, @Param("equipmentNo") String equipmentNo);
+
+	public List<UseEquipmentTitleVo> findAll(@Param("model") UseEquipmentPo useEquipmentPo);
+
+	@Select("select * from use_equipment_title where equipmentId=#{equipmentId} and fileId = #{fileId}")
+	public UseEquipmentTitlePo findOne(@Param("equipmentId") String  equipmentId,@Param("fileId") String  fileId);
+
+
+	@Delete("DELETE FROM use_equipment_title WHERE equipmentId = #{equipmentId}")
+	public void deleteEquipmentId(@Param("equipmentId") String  equipmentId);
+
+	@Delete("DELETE FROM use_equipment_title WHERE equipmentId = #{equipmentId} and fileId = #{fileId}")
+	public void deleteEquipmentId(@Param("equipmentId") String  equipmentId,@Param("fileId") String  fileId);
+
+
+}
